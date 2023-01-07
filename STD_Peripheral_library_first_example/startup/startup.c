fb30413e6b69ac973072c2979699c93790ff3fbe
Javi JaP <occam25@yahoo.es>
2023-01-07 20:21:12 +0100

    STD perh lib and CMSIS gpio and uart example

---
+++ STD_Peripheral_library_first_example/startup/startup.c
@@ -0,0 +1,147 @@
+
+#include <stdint.h>
+
+extern uint32_t _estack;
+extern uint32_t _sidata;
+extern uint32_t _sdata;
+extern uint32_t _edata;
+extern uint32_t _sbss;
+extern uint32_t _ebss;
+
+int main(void);
+
+void Default_Handler(void)
+{
+    while(1);
+}
+
+void Reset_Handler(void)
+{
+
+  uint32_t *psrc = &_sidata;
+  uint32_t *pdst = &_sdata;
+
+  while(pdst < &_edata)
+  {
+    *pdst = *psrc;
+    ++psrc;
+    ++pdst;
+  }
+
+  pdst = &_sbss;
+  while(pdst < &_ebss)
+  {
+    *pdst = 0;
+    ++pdst;
+  }
+
+  main();
+
+  while(1);
+}
+
+
+uint32_t vector_table[] __attribute__ ((section(".isr_vector"))) =
+{
+  (uint32_t)&_estack,
+  (uint32_t)&Reset_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  0,
+  0,
+  0,
+  0,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  0,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler,
+  0,
+  (uint32_t)&Default_Handler,
+  (uint32_t)&Default_Handler
+
+};
+
+
